6f2c8526a2d4c709af3207b5b98262b232e86133
NoelVega126 <noeljr126@gmail.com>
2022-10-29 12:00:18 -0500

    fixed msg position

--- src/pages/blog/chat.tsx
+++ src/pages/blog/chat.tsx
@@ -49,15 +49,39 @@ const Chat: React.FunctionComponent<IChatProps> = ({
       <ul className=' col flex w-full grow flex-col gap-2 p-2'>
         {messages?.map((message) => {
           return (
-            <li key={Math.random()} className='flex gap-2'>
+            <li
+              key={Math.random()}
+              className={` flex ${
+                message.username !== userCtx.username
+                  ? 'justify-start'
+                  : 'justify-end gap-2'
+              }`}
+            >
               <div>
-                <Image
-                  src={message.avatar}
-                  alt='user avatar'
-                  width='25'
-                  height='25'
-                  className='rounded-full'
-                />
+                {!message.avatar ? (
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='h-6 w-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z'
+                    />
+                  </svg>
+                ) : (
+                  <Image
+                    src={message.avatar}
+                    alt='user avatar'
+                    width='25'
+                    height='25'
+                    className='rounded-full'
+                  />
+                )}
               </div>
               <p className='w-fit rounded-lg bg-slate-400 p-2 font-semibold'>
                 {message.message}
